9306165589730774adb5ded5a1cd473432148cbf
cookiemonster0921 <142496173+cookiemonster0921@users.noreply.github.com>
2024-05-27 17:28:15 +1000

    Update tracking.js

--- tracking.js
+++ tracking.js
@@ -1,23 +1,21 @@
-console.log('hello there')
+console.log('hey there')
 
 document.addEventListener('DOMContentLoaded', () => {
   if (localStorage.visited) {
     localStorage.visited = Number(localStorage.visited)+1;
-  } 
-  else {
+  } else {
     localStorage.visited = 1;
   }
-  var string = `url-visited: ${document.URL} \n user-agent:   ${navigator.userAgent} \n times-visited: ${localStorage.visited}`
-  data = {content:string}
-  const requestOptions = {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: data,
-  };
-
-  fetch("https://discord.com/api/webhooks/1244270751159422996/a4EUkyAkKXb8GJi5hL8jNg9HFNwrNBdACSsiDpYMJym0CLavvj4iYrw2fjIfyEHQvAt4?", requestOptions)
-  .then((response) => response.text())
-  .then((result) => console.log(result))
-  .catch((error) => console.error(error));
-
+  var string = `url-visited: ${document.URL} \n user-agent: ${navigator.userAgent} \n times-visited: ${localStorage.visited}`
+  data = {content:string, }
+  fetch('https://discord.com/api/webhooks/1244270751159422996/a4EUkyAkKXb8GJi5hL8jNg9HFNwrNBdACSsiDpYMJym0CLavvj4iYrw2fjIfyEHQvAt4', {
+     method: "POST", // *GET, POST, PUT, DELETE, etc.
+     headers: {
+       "Content-Type": "application/json",
+       // 'Content-Type': 'application/x-www-form-urlencoded',
+     },
+     body: JSON.stringify(data), // body data type must match "Content-Type" header
+   }).then(() => {
+      console.log('sent')
+   })
 })
